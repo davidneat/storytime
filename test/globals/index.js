1cd031f28100bcc5fac425ea1b8e1e72d55f7682
David Neubauer <davidneub@gmail.com>
2016-05-08 21:35:45 +0200

    Add React and ReactTestUtils to globals

--- test/globals/index.js
+++ test/globals/index.js
@@ -1,5 +1,9 @@
 import assert from 'assert';
 import sinon from 'sinon';
+import React from 'react';
+import ReactTestUtils from 'react/lib/ReactTestUtils';
 
 global.assert = assert;
 global.sinon = sinon;
+global.React = React;
+global.ReactTestUtils = ReactTestUtils;
