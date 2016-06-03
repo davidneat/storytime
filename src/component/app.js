a240443d2ed94e8c9b1c986a911b2830af8d244b
David Neubauer <davidneub@gmail.com>
2016-06-03 23:14:10 +0200

    Add proptypes validation

--- src/component/app.js
+++ src/component/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 const renderStory = (story, key) =>
   <li key={key}>{story}</li>
@@ -6,8 +6,13 @@ const renderStory = (story, key) =>
 
 const renderStories = (stories = []) => stories.map(renderStory);
 
-export default (props) => {
+export default function App(props) {
   const { story } = props.store;
   const stories = renderStories(story) || 'No Stories';
+
   return <ul>{stories}</ul>;
+}
+
+App.propTypes = {
+  store: PropTypes.object.isRequired
 };
