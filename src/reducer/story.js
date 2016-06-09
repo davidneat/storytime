ef8b67f7a145bbaa2945c8ef3c0a59c5334676f8
David Neubauer <davidneub@gmail.com>
2016-06-09 20:45:14 +0200

    Add action type to story reducer (#17)

--- src/reducer/story.js
+++ src/reducer/story.js
@@ -1,6 +1,8 @@
+import { ADD_STORY } from '../action/type';
+
 export default (state = [], action = null) => {
   switch (action.type) {
-    case 'ADD_STORY':
+    case ADD_STORY:
       return state.concat([action.payload]);
     default:
       return state;
