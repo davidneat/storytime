b986b2e6a84fc9375aba5a0aa5084e3b0bd441a5
David Neubauer <davidneub@gmail.com>
2016-06-12 00:53:08 +0200

    Add ref

--- src/component/input.js
+++ src/component/input.js
@@ -1,11 +1,15 @@
 import React, { PropTypes } from 'react';
 
-export default function Input(props) {
-  const { actions } = props;
-  const addStory = () => {
-    actions.addStory('bar');
+export default function Input({ actions }) {
+  let input;
+  const addStory = (event) => {
+    event.preventDefault();
+    actions.addStory(input.value);
+    input.value = '';
   };
-  return <form onSubmit={addStory}><input value="bar" /></form>;
+  return (<form onSubmit={addStory}>
+    <input ref={node => (input = node)} />
+  </form>);
 }
 
 Input.propTypes = {
