8ed2a6052ec55a0d6a5fdb34f4c065093afcae98
David Neubauer <davidneub@gmail.com>
2016-06-12 01:04:57 +0200

    Use const arrow function

--- src/component/input.js
+++ src/component/input.js
@@ -1,17 +1,19 @@
 import React, { PropTypes } from 'react';
 
-export default function Input({ actions }) {
-  let input;
+const Input = ({ actions }, input) => {
   const addStory = (event) => {
     event.preventDefault();
     actions.addStory(input.value);
     input.value = '';
   };
+
   return (<form onSubmit={addStory}>
     <input ref={node => (input = node)} />
   </form>);
-}
+};
 
 Input.propTypes = {
   actions: PropTypes.object.isRequired
 };
+
+export default Input;
