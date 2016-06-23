e13152e9e1055ce0e0d9745554eb88cd6cfd5bda
David Neubauer <davidneub@gmail.com>
2016-06-23 23:04:21 +0200

    Fix remove story state (#27)

--- src/reducer/story.js
+++ src/reducer/story.js
@@ -1,10 +1,12 @@
 import { ADD_STORY, REMOVE_STORY, REMOVE_STORIES } from '../action/type';
 
 const removeStoryFromState = (story, state) => {
-  if (state.indexOf(story) !== -1) {
-    state.splice(state.indexOf(story), 1);
-  }
-  return state;
+  const storyIndex = state.indexOf(story);
+  if (storyIndex === -1) return state;
+  return [
+    ...state.slice(0, state.indexOf(story)),
+    ...state.slice(state.indexOf(story) + 1)
+  ];
 };
 
 export default (state = [], action = null) => {
@@ -14,7 +16,7 @@ export default (state = [], action = null) => {
     case REMOVE_STORY:
       return removeStoryFromState(action.payload, state);
     case REMOVE_STORIES:
-      action.payload.map((story) => removeStoryFromState(story, state));
+      action.payload.map((story) => (state = removeStoryFromState(story, state)));
       return state;
     default:
       return state;
