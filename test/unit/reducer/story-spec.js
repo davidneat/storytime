da2bb41ff70d41dd1f5ed0a0cc79a8fd59653ef6
David Neubauer <davidneub@gmail.com>
2016-06-26 20:13:33 +0200

    Add story id

--- test/unit/reducer/story-spec.js
+++ test/unit/reducer/story-spec.js
@@ -3,32 +3,32 @@ import storyReducer from '../../../src/reducer/story';
 
 describe('Story Reducer', () => {
   it(`adds new story on ${ADD_STORY}`, () => {
-    const state = ['foo'];
+    const state = [{ id: 0, text: 'foo' }];
     const action = {
       type: ADD_STORY,
       payload: 'bar'
     };
 
-    assert.deepEqual(storyReducer(state, action), ['foo', 'bar']);
+    assert.deepEqual(storyReducer(state, action), [{ id: 0, text: 'foo' }, { id: 1, text: 'bar' }]);
   });
 
   it(`removes story on ${REMOVE_STORY}`, () => {
-    const state = ['foo', 'bar'];
+    const state = [{ id: 0, text: 'foo' }, { id: 1, text: 'bar' }];
     const action = {
       type: REMOVE_STORY,
-      payload: 'bar'
+      payload: { id: 1, text: 'bar' }
     };
 
-    assert.deepEqual(storyReducer(state, action), ['foo']);
+    assert.deepEqual(storyReducer(state, action), [{ id: 0, text: 'foo' }]);
   });
 
-  it(`removes stories on ${REMOVE_STORIES}`, () => {
-    const state = ['foo', 'bar', 'any'];
+  it.only(`removes stories on ${REMOVE_STORIES}`, () => {
+    const state = [{ id: 0, text: 'foo' }, { id: 1, text: 'bar' }, { id: 2, text: 'any' }];
     const action = {
       type: REMOVE_STORIES,
-      payload: ['foo', 'bar', 'other']
+      payload: [{ id: 0, text: 'foo' }, { id: 1, text: 'bar' }, { id: 3, text: 'other' }]
     };
 
-    assert.deepEqual(storyReducer(state, action), ['any']);
+    assert.deepEqual(storyReducer(state, action), [{ id: 2, text: 'any' }]);
   });
 });
