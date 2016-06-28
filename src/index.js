39d7d6650998da4560bdb4f9b0e0fb525d4c9026
David Neubauer <davidneub@gmail.com>
2016-06-28 20:31:31 +0200

    Refactor util import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { createStore } from 'redux';
 import createReducer from './reducer';
-import createId from './util/unique-id';
+import { createId } from './util';
 import { createRenderer } from './renderer';
 import createActions from './action';
 import createStyles from './style';
