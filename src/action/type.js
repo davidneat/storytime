da4e519cb13ea1736f3516b0ce8815520ebff254
David Neubauer <davidneub@gmail.com>
2016-07-06 19:18:12 +0200

    Add role reducer (#46)
    * Add action types for role
    
    * Add role reducer
    
    * Fix linting

--- src/action/type.js
+++ src/action/type.js
@@ -1,3 +1,6 @@
 export const ADD_STORY = 'ADD_STORY';
 export const REMOVE_STORY = 'REMOVE_STORY';
 export const REMOVE_STORIES = 'REMOVE_STORIES';
+
+export const ADD_ROLE = 'ADD_ROLE';
+export const REMOVE_ROLE = 'REMOVE_ROLE';
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import createStoryReducer from './story';
+import rolesReducer from './role';
 
 export default (createId) => combineReducers({
-  stories: createStoryReducer(createId)
+  stories: createStoryReducer(createId),
+  roles: rolesReducer
 });
